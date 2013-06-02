a6d103e0d2fb84add4440da3216182d8ded6a5ce
Paritosh Piplewar <paritoshparitosh57@gmail.com>
2013-06-02 19:02:08 +0530

    update readme and dependecy

--- README.md
+++ README.md
@@ -2,24 +2,31 @@
 
 The official fedena installation guide is not fully documented.So,here i have released fully working installation script.I have tested in **Ubuntu Server 12.04.1 LTS 64 bit  in AWS-ec2** and it is working fine. 
 
-##Features 
-* RVM Support
-* Apache with Phussion Passenger 
-* Fully Automated
 
-##Installation 
+##Installation
+  
  
 ### For ubuntu or Debian
 1. Just ssh to your server , make a/c say `deployer` with `sudo` permission .
 2. Run the below command in home directory
+  1. First install the  required fedena dependency . 
 
-       
-        curl https://raw.github.com/passion8/fedena-install/master/install.sh | bash
+            curl https://raw.github.com/passion8/fedena-install/easy-install/required.sh | sudo bash
+
+  2. Now, install fedena finally
+
+            curl https://raw.github.com/passion8/fedena-install/easy-install/fedena-related.sh | sudo bash
 
 
 The above command will take you straight to `Phussion Passenger installation` with fedena in `~/fedena` 
 
-### if you're having some problem , follow 
+### You can also install it with RVM 
+ please go to  [with_rvm](https://github.com/passion8/fedena-install/tree/with_rvm) branch.
+## Caveats 
+ 1. This installation will remove all your installed gems .
+ 2. This gem will downgrade `rubygems` . 
+
+## If you're having some problem , follow 
   
     http://paritosh.passion8.co.in/post/49878771105/fedena-installation-guide
   
--- fedena-related.sh
+++ fedena-related.sh
@@ -10,10 +10,11 @@ control_c()
   exit 1
 }
 trap control_c SIGINT
-sudo  apt-get remove rubygems
 wget http://production.cf.rubygems.org/rubygems/rubygems-1.3.7.tgz
-tar xzf rubygems-1.3.7.tgz &&  sudo ruby rubygems-1.3.7/setup.rb
+tar xzf rubygems-1.3.7.tgz
+sudo ruby rubygems-1.3.7/setup.rb --no-format-executable
 wget http://www.projectfedena.org/download/fedena-github -O fedena.zip
+unzip fedena.zip && mv projectfedena-fedena* fedena/  && cd fedena/
 echo "gem: --no-ri --no-rdoc" >> ~/.gemrc
 sudo gem install bundler
 sudo gem install rails -v 2.3.5 
@@ -22,10 +23,17 @@ sudo gem install  declarative_authorization -v 0.5.1
 sudo gem install  mysql -v 2.8.1 
 sudo gem install  rake -v 0.8.7 
 sudo gem install  rush -v 0.6.8 
+sudo gem install passenger
 sudo apt-get -y install libmysqlclient-dev  mysql-server mysql-client
 read  -p "Please tell your root password,it is required for fedena installation: " password
 wget https://raw.github.com/passion8/fedena-install/master/ymlwriter.rb 
 ruby  ymlwriter.rb $password && rm ymlwriter.rb && background_color "database.yml updated"
-sudo rm -rf /var/lib/gems/1.8/gems/rake-10.0.4/ 
-sudo rm -rf /var/lib/gems/1.8/cache/rake-10.0.4.gem
-sudo rm -rf /var/lib/gems/1.8/specifications/rake-10.0.4.gemspec
+sudo rm -rf /usr/lib/ruby/gems/1.8/gems/rake-10.0.4/ 
+sudo rm -rf /usr/lib/ruby/gems/1.8/cache/rake-10.0.4.gem
+sudo rm -rf /usr/lib/ruby/gems/1.8/specifications/rake-10.0.4.gemspec
+sudo echo "export RAILS_ENV=production" >> ~/.bashrc
+source ~/.bashrc
+rake db:create
+chmod +x script/*
+rake fedena:plugins:install_all
+sudo passenger-install-apache2-module
